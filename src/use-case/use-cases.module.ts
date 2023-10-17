fe3aeffdd2ae6aacff89aadc49f4aae9f9822302
ThomDeV-bit <120723852+ThomDeV-bit@users.noreply.github.com>
2023-10-17 11:02:32 -0300

    feat/ thomaz - user use case context

---
+++ src/use-case/use-cases.module.ts
@@ -0,0 +1,16 @@
+import { DynamicModule, Module } from "@nestjs/common";
+import { USER_USE_CASE } from "./user";
+
+@Module({})
+
+export class UseCaseModule {
+    static register(): DynamicModule{
+        return {
+            module : UseCaseModule,
+            global : true,
+            imports : [...USER_USE_CASE],
+            exports : [...USER_USE_CASE]
+
+        }
+    }
+}
---
+++ src/use-case/user/index.ts
@@ -0,0 +1,3 @@
+import { UserUseCase } from "./user-create";
+
+export const USER_USE_CASE = [UserUseCase]
