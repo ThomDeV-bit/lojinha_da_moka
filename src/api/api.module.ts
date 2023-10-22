580ff6704977b170a8646d5b43546847417de561
ThomDeV-bit <120723852+ThomDeV-bit@users.noreply.github.com>
2023-10-22 10:28:47 -0400

    feat / thomaz - global response

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,22 +1,23 @@
-import { DynamicModule, Module, UsePipes } from "@nestjs/common";
-import { OPTIONS_TYPE } from "./api.module-definition"
-import { APP_GUARD, APP_INTERCEPTOR } from "@nestjs/core";
-import { UserController } from "./controllers/user.controller";
-import { AuthGuard } from "src/common/auth/auth.guards";
+import { DynamicModule, Module, UsePipes } from '@nestjs/common';
+import { OPTIONS_TYPE } from './api.module-definition';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import { UserController } from './controllers/user.controller';
+import { AuthGuard } from 'src/common/auth/auth.guards';
+import { GlobalResponse } from 'src/common/globlaResponse/global-response';
 @Module({})
-
 export class ApiModule {
-    static register(options: typeof OPTIONS_TYPE): DynamicModule {
+    static register (options: typeof OPTIONS_TYPE): DynamicModule {
         return {
             module: ApiModule,
-            controllers: [UserController],
+            controllers: [ UserController ],
             global: true,
-            imports: [options.useCaseModule],
+            imports: [ options.useCaseModule ],
             providers: [
                 {
-                    provide: APP_GUARD,
-                    useClass: AuthGuard,
-                }]
-        }
+                    provide: APP_INTERCEPTOR,
+                    useClass: GlobalResponse
+                }
+            ]
+        };
     }
 }
