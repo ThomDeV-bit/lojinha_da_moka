5c045a6bb7a7db666dbb1bfb50e63a20871b5bd0
ThomDeV-bit <120723852+ThomDeV-bit@users.noreply.github.com>
2023-10-22 10:28:30 -0400

    feat / thomaz - pino logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,23 +3,25 @@ import { TypeormModule } from './database/reposiotory/typeorm-module';
 import { RepositoryModule } from './database/reposiotory/repository.module';
 import { UseCaseModule } from './use-case/use-cases.module';
 import { ApiModule } from './api/api.module';
+import { LoggerModule } from 'nestjs-pino';
+import { logger } from './common/logger/logger';
 
 @Module({})
-
 export class AppModule {
-  static register(): DynamicModule {
+    static register (): DynamicModule {
+        const imports = [
+            LoggerModule.forRoot({
+                pinoHttp: logger
+            }),
+            TypeormModule.register(RepositoryModule.register()),
+            ApiModule.register({
+                useCaseModule: UseCaseModule.register()
+            })
+        ];
 
-    const imports = [
-      TypeormModule.register(RepositoryModule.register()),
-      ApiModule.register({
-        useCaseModule: UseCaseModule.register()
-      }
-      )
-    ]
-
-    return {
-      module: AppModule,
-      imports,
+        return {
+            module: AppModule,
+            imports
+        };
     }
-  }
 }
